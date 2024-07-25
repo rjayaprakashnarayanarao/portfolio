92d2743f895cfafa7d6b6093924e321ce749d814
JayaPrakashNarayanaRao <ravijayaprakashnarayanarao@gmail.com>
2024-07-26 01:43:02 +0530

    added new project

--- data/projects.js
+++ data/projects.js
@@ -1,12 +1,5 @@
 const projects = [
   {
-    name: "Colloge Website",
-    description: "Developed using HTML, CSS, JavaScript, Node.js, and Express.js, this application provides a comprehensive solution for monitoring and evaluating student performance.",
-    image: "images/projects/project-1.jpg",
-    link: "https://college-dashboard-8k8e.onrender.com/",
-    gitLink: "https://github.com/muke2110/Dashboard"
-  },
-  {
     name: "Basic game",
     description: "It is a classic hand game this project demonstrates the development of a basic yet engaging Rock Paper Scissors game using HTML, CSS, and JS.",
     image: "images/projects/project-2.png",
@@ -15,9 +8,23 @@ const projects = [
   },
   {
     name: "Streammer",
-    description: "Video streamming Website Developed using html css js",
+    description: "video streaming website built with HTML, CSS, and JavaScript! Featuring a sleek design, responsive layout, and smooth playback for an enhanced viewing experience.",
     image: "images/projects/project-3.jpg",
     link: "https://jayaprakashnarayanarao.github.io/video-streaming-website/",
     gitLink: "https://github.com/JayaPrakashNarayanaRao/video-streaming-website"
+  },
+  {
+    name: "BuyIt",
+    description: "responsive e-commerce website using HTML, CSS, and JavaScript! Features include dynamic UI, modern design, and seamless user experience.",
+    image: "images/projects/project-4.jpg",
+    link: "https://jayaprakashnarayanarao.github.io/e-commerce-website/",
+    gitLink: "https://github.com/JayaPrakashNarayanaRao/e-commerce-website"
+  },
+  {
+    name: "Colloge Website",
+    description: "Developed using HTML, CSS, JavaScript, Node.js, and Express.js, this application provides a comprehensive solution for monitoring and evaluating student performance.",
+    image: "images/projects/project-1.jpg",
+    link: "https://college-dashboard-8k8e.onrender.com/",
+    gitLink: "https://github.com/muke2110/Dashboard"
   }
 ];
--- script/home.js
+++ script/home.js
@@ -1,18 +1,45 @@
-let projectsHTML = '';
+let projectsToShow = 3; // initial number of projects to show
+let currentProjects = 0; // keep track of current projects shown
+let seeMoreBtn = document.querySelector('.js-see-more');
 
-projects.forEach((project) => {
-  projectsHTML += `
-    <div class="work">
-      <img src="${project.image}" alt="img">
-      <div class="layer">
-        <h3>${project.name}</h3>
-        <p>${project.description}</p>
-        <div class="link-container">
-          <a href="${project.link}" target="_blank"><i class="fa-solid fa-link"></i></a>
-          <a href="${project.gitLink}" target="_blank"><i class="fa-brands fa-github"></i></a>
+function renderProjects(projects) {
+  let projectsHTML = '';
+  projects.forEach((project, index) => {
+    if (index < projectsToShow) {
+      projectsHTML += `
+        <div class="work">
+          <img src="${project.image}" alt="img">
+          <div class="layer">
+            <h3>${project.name}</h3>
+            <p>${project.description}</p>
+            <div class="link-container">
+              <a href="${project.link}" target="_blank"><i class="fa-solid fa-link"></i></a>
+              <a href="${project.gitLink}" target="_blank"><i class="fa-brands fa-github"></i></a>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  `;
+      `;
+      currentProjects++;
+    }
+  });
+  document.querySelector('.js-work-list').innerHTML = projectsHTML;
+}
+
+renderProjects(projects);
+
+seeMoreBtn.addEventListener('click', () => {
+  if (seeMoreBtn.textContent === 'See More') {
+    projectsToShow += 3; // increment by 3 projects
+    currentProjects = 0;
+    renderProjects(projects);
+    if (currentProjects >= projects.length) {
+      seeMoreBtn.textContent = 'Show Less';
+    }
+  } else {
+    projectsToShow = 3;
+    currentProjects = 0;
+    renderProjects(projects);
+    seeMoreBtn.textContent = 'See More';
+    document.querySelector('.js-my-work').scrollIntoView({ behavior: 'smooth' });
+  }
 });
-document.querySelector('.js-work-list').innerHTML = projectsHTML;
